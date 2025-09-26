18b625cd631acd0dfd19f8d08dfe50e029795470
Shreeya <shreeyap@metropolia.fi>
2025-09-26 16:30:48 +0300

    Iteration 4

--- cm2-starter/frontend-simplified/src/hooks/useSignup.js
+++ cm2-starter/frontend-simplified/src/hooks/useSignup.js
@@ -1,34 +1,31 @@
-import { useState } from "react";
+
 import { toast } from "react-toastify";
 
 export const useSignup = () => {
 
     const signup = async(newUser)=> {
         try {
-            const res = await fetch("/api/users/signup", {
+            const res = await fetch("http://localhost:4000/users/signup", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
-            body: JSON.stringify(newUser),
+                body: JSON.stringify(newUser),
         });
 
       const data = await res.json();
       
-      if (res.ok) {
-        localStorage.setItem("data", JSON.stringify(data));
-        return true;
-      } else {
-        toast.error(data.message || "Signup failed");
-        return false;
-      }
+      if (!res.ok) throw new Error(data.message || "Signup failed");
 
+      localStorage.setItem("user", JSON.stringify(data.user));
+      localStorage.setItem("token", data.token);
+
+      return data; 
     } catch (error) {
-      console.error(error);
-      toast.error("Signup failed.Please try again.");
-      return false;
+      toast.error(error.message || "Signup failed. Please try again.");
+      return null;
     }
   };
 
-  return{signup};
+  return { signup };
 };
