48fd3bf6eb1a9e47eecc6b245f070359c4bdc752
Noah Stewart <natstrawets@gmail.com>
2025-09-26 22:00:59 +0300

    all is resolved

--- cm2-starter/frontend-simplified/src/pages/Login.jsx
+++ cm2-starter/frontend-simplified/src/pages/Login.jsx
@@ -14,7 +14,7 @@ const Login = ({ setIsAuthenticated }) => {
     setLoading(true);
 
     try {
-      const res = await fetch("/api/users/login", {
+      const res = await fetch("http://localhost:4000/users/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
