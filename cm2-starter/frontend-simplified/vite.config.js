93bc2a0749c089b0ad7e43545708bef069bb03e3
Noah Stewart <natstrawets@gmail.com>
2025-09-26 16:28:06 +0300

    adding things

--- cm2-starter/frontend-simplified/vite.config.js
+++ cm2-starter/frontend-simplified/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,15 +7,14 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
+      "/api": {
+        target: "https://w6-cm2.onrender.com/",
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
 });
-
 
 // export default defineConfig({
 //   plugins: [react()],
