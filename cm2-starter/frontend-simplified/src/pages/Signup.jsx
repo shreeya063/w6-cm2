dbcf3bbd8e7ba5007333ae70290929cfb14770be
Shreeya <shreeyap@metropolia.fi>
2025-09-26 17:38:53 +0300

    Changes-for-commit

--- cm2-starter/frontend-simplified/src/pages/Signup.jsx
+++ cm2-starter/frontend-simplified/src/pages/Signup.jsx
@@ -13,7 +13,7 @@ const SignupPage = ({setIsAuthenticated}) => {
   const [phone, setPhone] = useState("");
   const [gender, setGender] = useState("Male");
   const [dob, setDob] = useState("");
-  const [membership, setMembership] = useState("Free");
+  const [membership, setMembership] = useState("VIP");
 
   const submitForm = async (e) => {
     e.preventDefault();
